6009ffbe13a37ac993466a356891478b18eb8f12
Erick Lasluisa <erickl1709@gmail.com>
2022-11-23 12:28:04 -0500

    Creation of class Measurement and class DeadlineDate

--- 06-Code/FashionStore/src/fashionstore/model/DeadlineDate.java
+++ 06-Code/FashionStore/src/fashionstore/model/DeadlineDate.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fashionstore.model;
 
 /**
  *
- * @author Miguel Gutierrez, Pythons, DCCO-ESPE
+ * @author Erick Lasluisa, Pythons, DCCO-ESPE
  */
 public class DeadlineDate {
-    
+
+    private String deadlineDate;
+
+    @Override
+    public String toString() {
+        return "DeadlineDate{" + "deadlineDate=" + getDeadlineDate() + '}';
+    }
+
+    public DeadlineDate(String deadlineDate) {
+        this.deadlineDate = deadlineDate;
+    }
+
+    public DeadlineDate() {
+        this.deadlineDate = "";
+    }
+
+    /**
+     * @return the deadlineDate
+     */
+    public String getDeadlineDate() {
+        return deadlineDate;
+    }
+
+    /**
+     * @param deadlineDate the deadlineDate to set
+     */
+    public void setDeadlineDate(String deadlineDate) {
+        this.deadlineDate = deadlineDate;
+    }
+
 }
